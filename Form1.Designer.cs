82fe9e45be56fb064f8bceb62b881295a56a869c
arturjackowiak <80129605+arturjackowiak@users.noreply.github.com>
2021-05-11 16:59:18 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace VisualCryptography
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFile = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFile
+            // 
+            this.openFile.Location = new System.Drawing.Point(256, 12);
+            this.openFile.Name = "openFile";
+            this.openFile.Size = new System.Drawing.Size(73, 21);
+            this.openFile.TabIndex = 1;
+            this.openFile.Text = "Otwórz";
+            this.openFile.UseVisualStyleBackColor = true;
+            this.openFile.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(337, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 21);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Szyfruj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(418, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 21);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Deszyfruj";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(497, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(237, 211);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(13, 39);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(237, 211);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(256, 39);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(237, 211);
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(185, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 262);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.openFile);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button openFile;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VisualCryptography
+{
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Bitmap original;
+        private Bitmap output1;
+        private Bitmap output2;
+        private List<List<Color>> patterns = new List<List<Color>>//6
+        {
+            new List<Color> { Color.White, Color.White, Color.Black, Color.Black }, 
+            new List<Color> { Color.White, Color.Black, Color.White, Color.Black }, 
+            new List<Color> { Color.White, Color.Black, Color.Black, Color.White }, 
+            new List<Color> { Color.Black, Color.White, Color.White, Color.Black }, 
+            new List<Color> { Color.Black, Color.White, Color.Black, Color.White }, 
+            new List<Color> { Color.Black, Color.Black, Color.White, Color.White }, 
+        };
+        private static int[] pattern= new int[4];
+        Random random = new Random();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\Users\R2R2\Desktop",
+                Title = "Wybierz Obraz",
+                CheckFileExists = true,
+                CheckPathExists = true,
+                RestoreDirectory = true,
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                original = new Bitmap(textBox1.Text);
+            }
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            output1 = new Bitmap(original.Width * 2, original.Height * 2);
+            output2 = new Bitmap(original.Width * 2, original.Height * 2);
+            if (!string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                Color color;
+                for (int i = 0; i < original.Width; i++)
+                for (int j = 0; j < original.Height; j++)
+                    {
+                        color = original.GetPixel(i, j);
+                        for (int x = 0; x < 4; x++) pattern[x] = random.Next(0, 5);
+
+                        output1.SetPixel(i * 2, j * 2, patterns[pattern[0]][0]);
+                        output1.SetPixel(i * 2 + 1, j * 2, patterns[pattern[1]][1]);
+                        output1.SetPixel(i * 2, j * 2 + 1, patterns[pattern[2]][2]);
+                        output1.SetPixel(i * 2 + 1, j * 2 + 1, patterns[pattern[3]][3]);
+
+                        if (color.Name == "ff000000")
+                        {
+                            output2.SetPixel(i * 2, j * 2, patterns[Math.Abs(pattern[0] - 5)][0]);
+                            output2.SetPixel(i * 2 + 1, j * 2, patterns[Math.Abs(pattern[1] - 5)][1]);
+                            output2.SetPixel(i * 2, j * 2 + 1, patterns[Math.Abs(pattern[2] - 5)][2]);
+                            output2.SetPixel(i * 2 + 1, j * 2 + 1, patterns[Math.Abs(pattern[3] - 5)][3]);
+                        }
+                        else
+                        {
+                            output2.SetPixel(i * 2, j * 2, patterns[pattern[0]][0]);
+                            output2.SetPixel(i * 2 + 1, j * 2, patterns[pattern[1]][1]);
+                            output2.SetPixel(i * 2, j * 2 + 1, patterns[pattern[2]][2]);
+                            output2.SetPixel(i * 2 + 1, j * 2 + 1, patterns[pattern[3]][3]);
+                        }                       
+                    }
+            }
+            pictureBox1.Image = output1;
+            pictureBox2.Image = output2;
+            
+        }
+
+        public static Bitmap Decrypt(Bitmap o1, Bitmap o2)
+        {
+            Bitmap output = new Bitmap(o1.Width, o2.Height);
+            for (int width = 0; width < o1.Width; width++)
+                for (int height = 0; height < o1.Height; height++)
+                {
+                    if ((o1.GetPixel(width, height).Name == "ff000000" || o2.GetPixel(width, height).Name == "ff000000"))
+                    {
+                        output.SetPixel(width, height, Color.Black);
+                    }
+                    else
+                        output.SetPixel(width, height, Color.White);
+                }
+            return output;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox3.Image = Decrypt(output1, output2);
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
